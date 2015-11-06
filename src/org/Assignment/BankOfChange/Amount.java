faafdbcda7cf249f4a62711de65346e6502ca418
vishal <vishualhat@gmail.com>
2015-11-06 19:30:47 +0530

    2

--- src/org/Assignment/BankOfChange/Amount.java
+++ src/org/Assignment/BankOfChange/Amount.java
@@ -1,6 +1,8 @@
 package org.Assignment.BankOfChange;
+
 import java.util.HashMap;
 import java.util.Map;
+
 public class Amount {
 	HashMap<Integer, Integer> notes = new HashMap<Integer, Integer>();
 	public void BankerAddsMoneyToMachine() {
@@ -8,38 +10,39 @@ public class Amount {
 		am.addMoney();
 	}
 	public void addMoney() {
-
 		notes.put(10, 100);
 		notes.put(50, 50);
 		notes.put(100, 25);
 		notes.put(500, 20);
 		notes.put(1000, 10);
 	}
-
-	public void AcceptNoteFromCustomer(int note) throws Exception {
-		if (note < 10) {
-			showError("Maximum possible denomination value is 1000 Rs.");
-			//	System.out.println("We do not have Change available for this currency note");
+	public boolean AcceptNoteFromCustomer(int note) throws Exception {
+		if (note <= 10) {
+			showError("Not available for suchh Low Value");
+			return true;
+			// System.out.println("We do not have Change available for this
+			// currency note");
 		} else {
 			dispatch(note);
+			return false;
 		}
+		
 	}
 	private void showError(String string) throws Exception {
-		        throw new Exception(string);
+		throw new Exception(string);
 	}
-	private void dispatch(int note) {
+	private void dispatch(int note) throws Exception {
 		int n = note;
-		if (notes.containsKey(n))
-		{
+		if (notes.containsKey(n)) {
 			noteCalculator(n);
 			int newcount = (notes.get(n) + 1);
 			notes.replace(n, newcount);
 		}
 	}
-	private Map<Integer,Integer> noteCalculator(int n) {
+	public Map<Integer, Integer> noteCalculator(int n) throws Exception {
 		Map<Integer, Integer> dispatchedNotes = new HashMap<Integer, Integer>();
 
-		if (n == 1000) {
+		if (n == 1000 && checkifNotesAvailable()) {
 			notes.replace(500, notes.get(500) - 1);
 			notes.replace(100, notes.get(100) - 4);
 			notes.replace(50, notes.get(50) - 1);
@@ -49,7 +52,7 @@ public class Amount {
 			dispatchedNotes.put(50, 1);
 			dispatchedNotes.put(10, 5);
 		}
-		if (n == 500) {
+		if (n == 500 && checkifNotesAvailable()) {
 			notes.replace(100, notes.get(100) - 4);
 			notes.replace(50, notes.get(50) - 1);
 			notes.replace(10, notes.get(10) - 5);
@@ -58,7 +61,7 @@ public class Amount {
 			dispatchedNotes.put(50, 1);
 			dispatchedNotes.put(10, 5);
 		}
-		if (n == 100) {
+		if (n == 100 && checkifNotesAvailable()) {
 			notes.replace(50, notes.get(50) - 1);
 			notes.replace(10, notes.get(10) - 5);
 			dispatchedNotes.put(500, 1);
@@ -66,13 +69,22 @@ public class Amount {
 			dispatchedNotes.put(50, 1);
 			dispatchedNotes.put(10, 5);
 		}
-		if (n == 50) {
+		if (n == 50 && checkifNotesAvailable()) {
+
 			notes.replace(10, notes.get(10) - 5);
 			dispatchedNotes.put(500, 1);
 			dispatchedNotes.put(100, 4);
 			dispatchedNotes.put(50, 1);
 			dispatchedNotes.put(10, 5);
 		}
+		System.out.println(dispatchedNotes);
 		return dispatchedNotes;
 	}
+	public boolean checkifNotesAvailable() throws Exception {
+		if (!notes.containsValue(0)) {
+			return true;
+		} else
+			return false;
+	}
 }
+
--- src/org/Assignment/BankOfChange/TestAmount.java
+++ src/org/Assignment/BankOfChange/TestAmount.java
@@ -2,6 +2,10 @@ package org.Assignment.BankOfChange;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestAmount {
@@ -10,8 +14,27 @@ public class TestAmount {
 	public void Testaddmoney() {
 		Amount am = new Amount();
 		am.addMoney();
-		assertEquals()
+
+
 
 	}
 
+	@Test
+	public void TestnoteCalculator() throws Exception
+	{	Amount am = new Amount();
+	int n = 0;
+	am.noteCalculator(n);
+		Map<Integer, Integer> dispatchedNotes = new HashMap<Integer, Integer>();
+
+	
+		Assert.assertEquals ("{10,10}" , am.noteCalculator(100));
+	}
+	
+	@Test
+	public void testAcceptNoteFromCustomer() throws Exception
+	{
+		Amount am = new Amount();
+		Assert.assertEquals("Not available for suchh Low Value",am.AcceptNoteFromCustomer(10));
+	}
+	
 }
